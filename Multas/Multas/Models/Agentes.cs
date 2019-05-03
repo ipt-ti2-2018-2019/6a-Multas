cec0fb4f7cccb6cf6ac7ec1230ead51f26229e1e
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2019-05-03 19:26:17 +0100

    Adição de anotadores à classe dos Agentes, para validar o tipo e os dados introduzidos

--- Multas/Multas/Models/Agentes.cs
+++ Multas/Multas/Models/Agentes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,12 +13,25 @@ namespace Multas.Models {
       /// - esquadra
       /// - foto
 
+
+
+      [Key]
       public int ID { get; set; }
 
+      [Required(ErrorMessage = "o Nome é de preenchimento obrigatório.")]
+      [StringLength(40)]
+      [RegularExpression("[A-ZÁÉÍÓÚÂ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+(( | e | de | da | das | do | dos |-|'|d')[A-ZÁÉÍÓÚÂ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]*){1,3}",
+                         ErrorMessage ="só são aceites palavras, começadas por Maiúscula, " +
+                                       "separadas por um espeço em branco.")]
       public string Nome { get; set; }
 
+      [Required(ErrorMessage = "a Esquadra é de preenchimento obrigatório.")]
+      [StringLength(30)]
+      [RegularExpression("Torres Novas|Tomar|Entroncamento",
+                         ErrorMessage = " só é aceite Torres Novas ou Tomar ou Entroncamento")]
       public string Esquadra { get; set; }
 
+      [StringLength (30)]
       public string Fotografia { get; set; }
 
       // lista das multas associadas ao Agente
